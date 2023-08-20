181bb5e5619d788146fbf2a97f073b84430231be
Phuc <85390241+trinhgiaphuc24@users.noreply.github.com>
2023-08-20 19:59:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Đừng giận anh nữa nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    /* messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy */
+    messLink: 'https://www.facebook.com/giaphuc0402'
 }
